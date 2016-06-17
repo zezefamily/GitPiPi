baca973c45c3fb31b81e08e662da1f9b9f47cd84
吴文妹 <wuwenmei@kidcares.cn>
2016-06-17 10:44:31 +0800

    [task #17]修改背景色

--- GitToGitHub/GitToGitHub/ViewController.m
+++ GitToGitHub/GitToGitHub/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSString *pipi = @"pipi";
+    self.view.backgroundColor = [UIColor lightGrayColor];
 }
 
 - (void)didReceiveMemoryWarning {
